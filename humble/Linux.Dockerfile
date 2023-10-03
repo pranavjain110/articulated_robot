7346b2595e4fa19e41bd1fa378eeee76736789eb
pranavVention <pranav.jain@vention.cc>
2023-10-02 23:30:25 -0400

    cleanup

--- humble/Linux.Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM ros2-humble-base-image  as ros2-humble-image-built
-RUN colcon build --symlink-install
-
-# add sourcing the catkin workspace to .zshrc
-RUN echo 'source ~/workspace/install/setup.zsh' >> ~/.zshrc
--- humble/build-image-local.sh
+++ humble/build-image-local.sh
@@ -1,9 +1,6 @@
 docker pull ghcr.io/pranavjain110/ros2_docker/ros2-humble-image-built:latest
 docker tag ghcr.io/pranavjain110/ros2_docker/ros2-humble-image-built:latest ros2-humble-image-built
 
-
-
-# BASE_IMAGE="humble-image-temp"
 BUILT_IMAGE="humble-image-pc"
 
 echo "Building image from Dockerfile..."
