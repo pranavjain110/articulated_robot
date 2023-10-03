ca7f817188d990487c98b2f4438523a0446520c1
pranavVention <pranav.jain@vention.cc>
2023-10-02 22:51:14 -0400

    docker file added

---
+++ humble/build-image-local.sh
@@ -0,0 +1,11 @@
+docker pull ghcr.io/pranavjain110/ros2_docker/ros2-humble-image-built:latest
+docker tag ghcr.io/pranavjain110/ros2_docker/ros2-humble-image-built:latest ros2-humble-image-built
+
+
+
+# BASE_IMAGE="ros2-humble-image-built"
+BUILT_IMAGE="humble-image-pc"
+
+echo "Building image from Dockerfile..."
+echo "First DOCKER Build"
+docker build --no-cache . --build-arg GIT_PAT="$PAT" -t "$BUILT_IMAGE" || { echo "Error: Docker build failed"; exit 1; }
