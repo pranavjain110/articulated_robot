eb972fe20cdbcf639da05129b7c305fbe19d46bd
pranavVention <pranav.jain@vention.cc>
2023-10-08 11:22:25 -0400

    add command to remove dangling images after pulling latest

--- humble/pull-image-remote.sh
+++ humble/pull-image-remote.sh
@@ -1,2 +1,8 @@
 docker pull ghcr.io/pranavjain110/articulated_robot/humble-image-pc:latest
 docker tag ghcr.io/pranavjain110/articulated_robot/humble-image-pc:latest humble-image-pc
+
+DANGLING_IMAGES=$(docker images -f "dangling=true" -q)
+if [[ -n "$DANGLING_IMAGES" ]]; then
+	echo "Remove dangling images"
+    docker rmi -f $DANGLING_IMAGES || { echo "Error: Failed to remove dangling images"; exit 1; }
+fi
